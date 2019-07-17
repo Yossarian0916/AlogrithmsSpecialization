d029472643b11f35b77a2b09af9e87d164053149
Songmao Li <li.songmao1609@gmail.com>
2019-07-17 14:15:59 +0200

    refactor use list comprehension instead of loop, more pythonic

--- karger_mincut.py
+++ karger_mincut.py
@@ -2,30 +2,23 @@ from collections import defaultdict
 import random
 
 
-def karger_mincut(graph):
-    while len(graph.keys()) > 2:
+def karger_mincut(graph, min_v=2):
+    while len(graph.keys()) > min_v:
         u = random.choice(list(graph.keys()))
         v = random.choice(graph[u])
         merge(graph, u, v)
-
     return len(list(graph.values())[0])
 
 
 def merge(graph, u, v):
     graph[u] = graph[u] + graph[v]
-    # remove self-loop
-    while graph[u].count(v) > 0:
-        graph[u].remove(v)
-    while graph[u].count(u) > 0:
-        graph[u].remove(u)
     # merge endpoints, remove end vertex
     del graph[v]
-
     # replace vertex v with vertex u
-    for start in graph.keys():
-        for idx, end in enumerate(graph[start]):
-            if end == v:
-                graph[start][idx] = u
+    for key, value in graph.items():
+        graph[key] = [u if node == v else node for node in graph[key]]
+    # remove self-loop
+    graph[u] = [node for node in graph[u] if node != u]
 
 
 def main(graph, minimum=None):
@@ -39,11 +32,11 @@ def main(graph, minimum=None):
 
 
 if __name__ == "__main__":
-    adj_list = defaultdict(list)
+    graph = defaultdict(list)
     with open("kargerMinCut.txt", "r") as f:
         for line in f.readlines():
             line = list(map(int, line.split('\t')[:-1]))
             for i in range(1, len(line)):
-                adj_list[line[0]].append(line[i])
+                graph[line[0]].append(line[i])
 
-    print(main(adj_list))  # correct answer: 17
+    print(main(graph))  # correct answer: 17
