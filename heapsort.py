bb82b9f01f4610e19b2ef26fa5c2ad9e4f04bc38
Songmao Li <li.songmao1609@gmail.com>
2019-08-31 22:27:02 +0200

    remove heap.py, add build heap operations in heapsort.py

--- heapsort.py
+++ heapsort.py
@@ -1,13 +1,31 @@
-from heap import Heap
+def max_heapify(heap, idx, heapsize):
+    left = 2*idx+1
+    right = 2*idx+2
+    max_id = idx
+    if left <= heapsize-1 and heap[left] > heap[max_id]:
+        max_id = left
+    if right <= heapsize-1 and heap[right] > heap[max_id]:
+        max_id = right
+    if max_id != idx:
+        heap[idx], heap[max_id] = heap[max_id], heap[idx]
+        max_heapify(heap, max_id, heapsize)
+
+
+def build_max_heap(array):
+    mid_id = int(len(array)/2)-1
+    for i in range(mid_id, -1, -1):
+        max_heapify(array, i, len(array))
+    return array
 
 
 def heapsort(array):
-    heap = Heap.heapify(array, 'max')
+    heap = build_max_heap(array)
+    heapsize = len(array)
     for i in range(len(heap)-1, 0, -1):
         # move the maximum to the end of the array
-        heap.heap[0], heap.heap[i] = heap.heap[i], heap.heap[0]
-        heap.decrease_heapsize(1)
-        heap.max_heapify(0)  # float down the root
+        heap[0], heap[i] = heap[i], heap[0]
+        heapsize = heapsize - 1
+        max_heapify(heap, 0, heapsize)  # float down the root
     return heap
 
 
