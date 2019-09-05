f75371270390d30b0e77f773d0316209621184a6
Songmao Li <li.songmao1609@gmail.com>
2019-09-05 17:26:44 +0200

    correct queue implementation in queue.py

--- queue.py
+++ queue.py
@@ -1,25 +1,36 @@
 class Queue:
-    def __init__(self, buffersize):
-        self.size = buffersize
-        self.queue = [None]*buffersize
+    def __init__(self, length):
+        self.length = length
+        self.queue = [None]*(length+1)
         self.head = 0
         self.tail = 0
 
     def enqueue(self, x):
-        if (self.head+self.tail) % self.size == 0 and self.tail != 0:
+        if self.is_full():
             return 'Overflow'
-        self.queue[self.tail % (self.size)] = x
-        self.tail = self.tail + 1
+        self.queue[self.tail] = x
+        if self.tail == self.length:
+            self.tail = 0
+        else:
+            self.tail = self.tail + 1
 
     def dequeue(self):
         if self.is_empty():
             return 'Underflow'
-        x = self.queue[self.head % (self.size)]
-        self.head = self.head + 1
+        x = self.queue[self.head]
+        if self.head == self.length:
+            self.head = 0
+        else:
+            self.head = self.head + 1
         return x
 
     def is_empty(self):
         if self.head == self.tail:
+            return True
+        return False
+
+    def is_full(self):
+        if self.head == self.tail+1 or (self.head == 0 and self.tail == self.length):
             return True
         return False
 
@@ -30,9 +41,15 @@ if __name__ == "__main__":
     queue.enqueue(1)
     queue.enqueue(2)
     queue.enqueue(3)
+    print(queue.queue)
+    res = queue.enqueue(4)
+    print(res)
+    print(queue.queue)
+    print(queue.dequeue())
+    print(queue.dequeue())
+    print(queue.dequeue())
     queue.enqueue(4)
-    print(queue.dequeue())
-    print(queue.dequeue())
-    print(queue.dequeue())
-    print(queue.dequeue())
+    queue.dequeue()
+    queue.enqueue('string')
+    print(queue.queue)
     print(queue.is_empty())
