36e77bbe13fb4322a3621f1e21882be4fc73afbd
Songmao Li <li.songmao1609@gmail.com>
2019-11-02 22:40:32 +0100

    refactor median_in_time, found error in priority_queue implementation, need to debug

--- median_in_time.py
+++ median_in_time.py
@@ -1,44 +1,41 @@
-from priority_queue import Heapq
+import heapq
 
 
-def median_in_time(array, idx, median, lower, higher):
+def median_in_time(item, heap_low=[], heap_high=[]):
     """
-    find the median in a dynamic way, read the elements one by one, and then 
+    find the median in a dynamic way, read the elements one by one, and then
     return the median of the current subarray
     """
-    new_item = array[idx]
-    if idx == 0:
-        median = new_item
+    if len(heap_low) == 0:
+        heapq.heappush(heap_low, -item)
+    else:
+        median = -heap_low[0]
+        if item > median:
+            heapq.heappush(heap_high, item)
+            if len(heap_high) > len(heap_low):
+                tmp = heapq.heappop(heap_high)
+                heapq.heappush(heap_low, -tmp)
+        else:
+            heapq.heappush(heap_low, -item)
+            if len(heap_low) - len(heap_high) > 1:
+                tmp = heapq.heappop(heap_low)
+                heapq.heappush(heap_high, -tmp)
+    return -heap_low[0]
 
-    if new_item >= median:
-        higher.insert(new_item)
-    elif new_item < median:
-        lower.insert(new_item)
 
-    length = idx+1
-    if length % 2 != 0:
-        if len(lower) > len(higher):
-            median = lower.maximum()
-        elif len(higher) > len(lower):
-            median = higher.minimum()
-    if length % 2 == 0:
-        if len(lower) > len(higher):
-            tmp = lower.extract_max()
-            higher.insert(tmp)
-        elif len(higher) > len(lower):
-            tmp = higher.extract_min()
-            lower.insert(tmp)
-        # lower.maximum, higher.minimum are both the median
-        median = lower.maximum()
-
-    return median
+def median_maintenance(array):
+    medians = []
+    for item in array:
+        median = median_in_time(item)
+        medians.append(median)
+    return medians
 
 
 if __name__ == "__main__":
-    array = [5, 8, 2, 3, 7, 4, 1, 6, 9]
-    lower = Heapq([], 'max')
-    higher = Heapq([], 'min')
-    median = 0
-    for i in range(len(array)):
-        median = median_in_time(array, i, median, lower, higher)
-        print(median)
+    array = []
+    with open('Median.txt', 'r') as fd:
+        for line in fd:
+            array.append(int(line))
+
+    medians = median_maintenance(array)
+    print(sum(medians) % 10000)  # 1213
