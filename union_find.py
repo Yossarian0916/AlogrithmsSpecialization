442ca70a1955315b370a80261fca776cee393697
Songmao Li <li.songmao1609@gmail.com>
2020-01-02 22:23:48 +0100

    refactor disjoint set, finished unittest

--- union_find.py
+++ union_find.py
@@ -1,29 +1,33 @@
 class DisjointSet:
     """
-    represent disjoint sets by rooted trees, 
+    represent disjoint sets by rooted trees,
     each node with a rank and points to its parent only
     """
 
-    def __init__(self):
-        self.root = None
-        self.tree = list()
-        self.parent = dict()
-        self.rank = dict()
+    def __init__(self, array):
+        self.make_set(array)
+        self.count = len(array)  # num of different union sets
 
-    def make_set(self, data):
-        self.root = data
-        self.tree.append(data)
-        self.parent[data] = self.root
-        self.rank[data] = 0
+    def make_set(self, array):
+        self.parent = {item: item for item in array}
+        self.rank = dict.fromkeys(array, 0)
 
-    def find(self, x):
-        """pass up to find the root and pass back down to update each node to point directly to the root"""
+    def find_set(self, x):
+        """
+        path compression:
+        pass up to find the root
+        and pass back down to update each node to point directly to the root
+        """
         if x != self.parent[x]:
-            self.parent[x] = self.find(self.parent[x])
+            self.parent[x] = self.find_set(self.parent[x])
         return self.parent[x]
 
     def union(self, x, y):
-        return self.link(self.find(x), self.find(y))
+        if not self.connected(x, y):
+            # each union, the num of differents sets reduce self by 1
+            self.count = self.count - 1
+            return self.link(self.find_set(x), self.find_set(y))
+        return None
 
     def link(self, x, y):
         if self.rank[x] > self.rank[y]:
@@ -36,10 +40,12 @@ class DisjointSet:
                 self.rank[y] = self.rank[y] + 1
 
     def connected(self, x, y):
-        return self.find(x) == self.find(y)
+        return self.find_set(x) == self.find_set(y)
 
-    def count(self):
-        return len(self.tree)
+    @property
+    def num_unions(self):
+        """count how many different union sets"""
+        return self.count
 
 
 if __name__ == '__main__':
